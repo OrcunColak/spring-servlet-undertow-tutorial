f4aeb0f684fc6eebc9ceb5f7253455a6fff7744b
Orçun Çolak <orcun.colak@hazelcast.com>
2024-06-20 11:37:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Read Me 
+The original idea is from  
+https://medium.com/@haiou-a/spring-boot-how-many-requests-can-spring-boot-handle-simultaneously-a57b41bdba6a?source=explore---------0-109--------------------19772482_12ab_49ec_af1b_fb57b134f947-------15
+
+
---
+++ src/main/java/com/colak/springtutorial/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.colak.springtutorial.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    // http://localhost:8080
+    @GetMapping
+    String hello () {
+        return "Hello World!";
+    }
+}
